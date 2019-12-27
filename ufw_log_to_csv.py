831e29a900b0c2cb8066c58c5e0b1c8108ac987c
hms5232 <hms5232@yahoo.com.tw>
2019-12-27 17:27:00 +0800

    使用 csv package 來產生檔案 + 改寫get_expect_time及各種 debug
    其實前一個 commit 我就是一直寫沒有跑跑看呢
    
    中間踩了物件導向的坑，覺得抱歉QQ
    感謝 ricklai 學長點醒

--- ufw_log_to_csv.py
+++ ufw_log_to_csv.py
@@ -11,6 +11,7 @@ Windows users can download .exe file on https://github.com/hms5232/ufw-log-to-cs
 
 
 import datetime
+import csv
 
 
 def main():
@@ -23,11 +24,12 @@ def main():
 		now_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")  # 取得現在時間並格式化
 		# ※※ >>>  ↓↓↓↓↓↓↓↓↓↓↓ change output filename or path here if you want <<< ※※
 		with open("ufw"+now_time+".csv", 'a', encoding='UTF-8') as o:  # o of "output"
-			# TODO: 改用 csv 模組來做寫入的動作＆import
 			csv_title = '"月","日","時間","主機名稱","kernel 時間","動作","IN","OUT","MAC", "來自(src)", "DST", "LEN","TOS","PREC","TTL","ID","協定(PROTO)","來源埠(SPT)","DPT","WINDOW","RES","Control Bits / flags","URGP"\n'
-			fieldnames = ['月', '日', '時間', '主機名稱', 'kernel 時間', '動作', 'IN', 'OUT', 'MAC', 'SRC', 'DST', 'LEN', 'TOS', 'PREC', 'TTL', 'ID', 'DF', 'PROTO', 'SPT', 'DPT', 'WINDOW', 'RES', 'Control Bits / flags', 'URGP']  # 欄位名稱
-			output = csv_title  # 輸出到 csv 的內容
-			csv.DictWriter(o, fieldnames=fieldnames)  # 寫入標題
+
+			fieldnames = ['月', '日', '時間', '主機名稱', 'kernel 時間', '動作', 'IN', 'OUT', 'MAC', 'SRC', 'DST', 'LEN', 'TOS', 'PREC', 'TTL', 'ID', 'DF', 'PROTO', 'SPT', 'DPT', 'WINDOW', 'RES', 'Control Bits / flags', 'URGP', 'TC', 'HOPLIMIT', 'FLOWLBL', 'TYPE', 'CODE', 'SEQ', '??']  # 欄位名稱
+			writer = csv.DictWriter(o, fieldnames=fieldnames)
+			writer.writeheader()  # 寫入標題
+
 			# 讀取每一筆紀錄
 			for i in range(len(ufw_logs)):
 				record = dict()
@@ -39,87 +41,45 @@ def main():
 				# kernel 名稱
 				record['主機名稱'] = get_kernel_name(ufw_logs[i])
 				# 剩下的部份
-				output = output + get_expect_time(ufw_logs[i])  # TODO: dict
+				for j in get_expect_time(ufw_logs[i]).items():
+					record[j[0]] = j[1]
 
-			o.write(output)  # 寫入csv
+				writer.writerow(record)  # 寫入一列
+
 			print(end='\n\n')  # 讓之前 print 出來的東西不會被覆蓋（\r\n）順便排版
 
 
 # 抓取 kernel 之後的部分
 def get_expect_time(log_string):
 	log_string = log_string[16:]  # 把前面已經擷取的時間部分切掉
-	result = ''  # 擷取出來的內容
-	isDF = False  # 這筆紀錄是否為DF（don't fragment）的判斷
-	isUDP = False  # 判斷是不是UDP協定（如果是UDP協定會有些欄位沒有資料）
-	while log_string.find('[') != -1:  # 這筆紀錄還沒跑完
+	after_time_dict = dict()  # kernel 之後的部分
+	which_brackets = 1  # 第幾個中括號的資料囉
+
+	# 先抓中括號的部份
+	while log_string.find('[') != -1:  # 這筆紀錄中括號的部份還沒抓完
 		# Re:從零開始
 		starttag = 0
 		endtag = 0
 		if log_string.find('[') != -1:  # 先抓有[]的
 			starttag = log_string.find('[')
 			endtag = log_string.find(']')
-			result = result + '"' + log_string[starttag+1:endtag] + '",'  # TODO: 改用 csv 模組來做寫入的動作
+			# 抓取內容
+			if which_brackets == 1:
+				after_time_dict['kernel 時間'] = log_string[starttag+1:endtag]
+			elif which_brackets == 2:
+				after_time_dict['動作'] = log_string[starttag+1:endtag]
+			else:  # 以防萬一我也不知道會不會哪天更新就多一個中括號的項目
+				after_time_dict['??'] = log_string[starttag+1:endtag]
+			which_brackets += 1
 			log_string = log_string[endtag+1:]  # 去掉]後從空格開始
 	log_string = log_string[1:]  # 去掉開頭空格
+
 	# 中括號（方括號）抓完後 split 以空格為分隔符號切割剩下的字串
-	result = result + get_after_brackets(log_string)  # TODO: 改用 csv 模組來做寫入的動作——改接 dict
+	# 逐一放入字典
+	for i in get_after_brackets(log_string).items():
+		after_time_dict[i[0]] = i[1]
 
-	# 	elif log_string.find(' ') != -1:  # 再抓剩下的
-	# 		starttag = log_string.find(' ')
-	# 		log_string_tmp = log_string[starttag+1:]
-	# 		endtag = log_string_tmp.find(' ') + 1
-	# 		# 如果是有=的話就只抓=後面的部分
-	# 		ptr_equal = log_string[starttag+1:endtag].find('=')
-	# 		# 因為在RES之後flag有可能不只一個，所以在RES之後到URGP之間通通抓出來
-	# 		isRES = False
-	# 		if ptr_equal != -1:  # 有=
-	# 			if log_string[starttag+1:ptr_equal+1] == "RES":
-	# 				isRES = True
-	# 			starttag = log_string.find('=')
-	#
-	# 		if log_string[starttag+1:endtag] == 'UDP':  # 如果是UDP
-	# 			isUDP = True
-	#
-	# 		if log_string[starttag+1:endtag] == 'DF':  # 如果是DF
-	# 			isDF = True
-	# 			log_string = log_string[endtag:]  # 保留空格不切掉
-	# 			continue
-	#
-	# 		result = result + '"' + log_string[starttag+1:endtag] + '",'
-	#
-	# 		log_string = log_string[endtag:]  # 保留空格不切掉
-	# 		if isRES:
-	# 			# 抓取RES後面剩下的項目到URPG之前(control flags)
-	# 			tmp_index = 0
-	# 			# 擷取control flags和之後的內容
-	# 			tmp = log_string[1:].split(" ")  # 將剩下的字串用空格來分割並放進list
-	# 			while True:
-	# 				if tmp[tmp_index].find("=") >= 0:  # 如果發現=，代表已經擷取完畢了
-	# 					break
-	# 				tmp_index += 1
-	# 			S = ""
-	# 			for s in tmp[:tmp_index]:  # 開始拿出每個control flags
-	# 				S += s+"|"
-	# 			S = S[:-1]  # 拿掉最後一個|
-	# 			result = result + '"' + S + '",'
-	# 			# 將剩下的部分依照原本的邏輯處理
-	# 			ptr = log_string.find("=")
-	# 			while True:
-	# 				ptr -= 1
-	# 				if log_string[ptr] == " ":
-	# 					break
-	# 			log_string = log_string[ptr:]
-	#
-	#
-	# if isDF :  # 如果有DF就在最後面加上
-	# 	if isUDP:  # 如果是UDP封包則要補齊空出來的欄位
-	# 		result = result + '"","","","DF",'  # 補漏洞
-	# 	else:
-	# 		result = result + '"DF",'  # 一般情況下直接加在最後面即可
-
-	# TODO: return dict
-	result = result + '\n'  # 一筆紀錄處理完了記得換行
-	return result
+	return after_time_dict
 
 
 # 擷取 kernel 的名字
@@ -134,8 +94,9 @@ def get_kernel_name(log_string):
 # 抓取中括號之後的部份並切割處理
 def get_after_brackets(log_string):
 	log_list = log_string.split(' ')  # 用空格去分割紀錄個每個項目
-	log_dict = dict()
-	orig_log_list = log_list
+	log_list.remove('\n')  # 檔案裡面有換行符號先移除掉
+	log_dict = dict()  # 這筆紀錄中方括號後面分割好的部份
+	orig_log_list = log_list.copy()  # 複製請加上 copy 否則 reference 會指向同一個位址
 
 	for i in orig_log_list:  # 處理每一個項目
 		# 用等號分割
@@ -147,9 +108,11 @@ def get_after_brackets(log_string):
 			# 直接給定 key 後設定 value
 			# Control Bits / flags 另外處理不在這邊弄
 			if log_item[0] == 'DF':
-				log_dict['DF'] = '*'
+				log_dict['DF'] = '＊'
 			else:
-				log_dict['DF'] = ''
+				log_dict['DF'] = ''  # 避免找不到 key 的問題
+				# 除此之外大概就是 Control Bits / flags 的部份，直接跳過不移除
+				continue
 		log_list.remove(i)  # 跑完之後就移除項目
 
 	# 處理 Control Bits / flags
